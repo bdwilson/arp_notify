16199a207ed6927b4361e3d1761d50d7b04a978b
Brian Wilson <wilson@bubba.org>
2019-11-26 12:47:15 -0500

    add code to call webhooks on arrival/departure

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ need to know what was added without looking in dnsmasq log.
 
 Optional .arpignore file will not notify you about MAC addresses you "trust". 
 
+Optional .arpnotify file to trigger webhooks if someone arrives/leaves. This
+comes in handy with Hubitat and a [Virtual Presence
+Sensor](https://github.com/bdwilson/hubitat/blob/master/Geofency-Presence/virtual-mobile-presence.groovy).
+
 # Installation
 <code>$ ssh admin@10.0.0.1</code> (or whatever your router IP is)<br>
 <code>$ sudo su -; mkdir -p /config/arp_notify; cd /config/arp_notify</code><br>
@@ -32,3 +36,17 @@ A: Create a file called .arpignore and put the MAC address (or the full entry
 from .arptable file) into this file. Theoretically, you could copy .arptable to
 .arpignore and ignore all devices from the getgo, then only be notified about
 new things.
+
+Q: How does the webhook work. 
+A: Create a .arpnotify file.  The file should consist of a + or - depending on
+if the webhook is for arrival or departing. Then the MAC address of the device,
+then the webhook.  For instance:
+<pre>
+# more .arpnotify
+# User 1 
++ f0:99:b6:ab:cd:12 http://192.168.1.16/apps/8/devices/1669/on?access_token=abcd-1234-xxxx-xxxx-xxxx
+- f0:99:b6:ab:cd:12 http://192.168.1.16/apps/8/devices/1669/off?access_token=abcd-1234-xxxx-xxxx-xxxx
+# User 2 
++ 74:b5:87:ab:cd:12 http://192.168.1.16/apps/8/devices/1670/on?access_token=abcd-1234-xxxx-xxxx-xxxx
+- 74:b5:87:ab:cd:12 http://192.168.1.16/apps/8/devices/1670/off?access_token=abcd-1234-xxxx-xxxx-xxxx
+</pre>
--- arp_notify.sh
+++ arp_notify.sh
@@ -5,6 +5,12 @@
 # If there are "trusted" devices you wish to ignore, put the MAC address, or
 # their whole ARP table entry into a file called .arpignore
 #
+# If there are devices that need to trigger webhooks, then create a
+# .arpnotify file with this format for arriving:
+# + MAC URL
+# or this format for leaving:
+# - MAC URL
+#
 # To install: 
 # mkdir -p /config/arp_notify
 # cp backup.sh arp_notify.sh /config/arp_notify
@@ -12,10 +18,16 @@
 # 23 11 * * * /config/arp_notify/backup.sh
 # */5 * * * * /config/arp_notify/arp_notify.sh
 #
+# optional if you want to be notified via pushover if devices arrive/leave
+pushover_user_token=""
+pushover_app_token=""
+# if you're not using pushover, uncomment the line below.
+#logonly=1
+# These are specific to EdgeOS
 arpcommand="/opt/vyatta/bin/vyatta-op-cmd-wrapper show arp"
-pushover_user_token="<pushover_user_token>"
-pushover_app_token="<pushover_app_token"
-logonly=1  # comment this out to send pushover events.
+dhcpleases="/opt/vyatta/bin/vyatta-op-cmd-wrapper show dhcp leases"
+hostname=`hostname`
+service="ARP Monitor"
 dir="/var/log/arp_notify"
 
 if [ ! -d $dir ]
@@ -30,17 +42,34 @@ fi
 $arpcommand | grep -v 'incomplete' > $dir/.arptablenew
 touch $dir/.arplist
 echo "New ARP Entries" > $dir/.arplist
+count=1
 for newarp in $(diff $dir/.arptable $dir/.arptablenew | grep + | grep ether | sed 's/^+//g' | awk '{print $1}')
 do
+    if [ count -eq 1 ]
+    then
+	$dhcpleases > $dir/.dhcpleases
+ 	count=`expr $count + 1`
+    fi
+
+    if [ -f $dir/.arpnotify ]
+    then
+	new_mac=`grep "$newarp" $dir/.dhcpleases |  awk '{print $2}'`
+	for notify in $(grep "$new_mac" $dir/.arpnotify | grep -v "^#" | grep + | awk '{print $3}')
+	do
+		logger -t [arp_notify] "New Device Detected ($new_mac) -> Notifying $notify"
+		curl -s "$notify"
+	done
+    fi
+
     if [ -f $dir/.arpignore ]
     then
     	if [[ $(grep -L "$newarp" $dir/.arpignore) ]]; then
-		new_entry="$newarp - $(/opt/vyatta/bin/vyatta-op-cmd-wrapper show dhcp leases | grep "$newarp" | awk '{print $6}')"
+		new_entry="$newarp - $(grep "$newarp" $dir/.dhcpleases | awk '{print $6}')"
     		logger -t [arp_notify] "New Device Detected -> $new_entry"
     		echo $new_entry >> $dir/.arplist
     	fi
     else
-	new_entry="$newarp - $(/opt/vyatta/bin/vyatta-op-cmd-wrapper show dhcp leases | grep "$newarp" | awk '{print $6}')"
+	new_entry="$newarp - $(grep "$newarp" $dir/.dhcpleases | awk '{print $6}')"
     	logger -t [arp_notify] "New Device Detected -> $new_entry"
     	echo $new_entry >> $dir/.arplist
     fi
@@ -48,17 +77,33 @@ done
 
 echo "" >> $dir/.arplist
 echo "Removed ARP Entries" >> $dir/.arplist
+count=1
 for noarp in $(diff $dir/.arptable $dir/.arptablenew | grep - | grep ether | sed 's/^-//g' | awk '{print $1}')
 do
+    if [ count -eq 1 ]
+    then
+	$dhcpleases > $dir/.dhcpleases
+ 	count=`expr $count + 1`
+    fi
+
+    if [ -f $dir/.arpnotify ]
+    then
+	lost_mac=`grep "$noarp" $dir/.dhcpleases | awk '{print $2}'`
+	for notify in $(grep "$lost_mac" $dir/.arpnotify | grep -v "^#" | grep - | awk '{print $3}')
+	do
+		logger -t [arp_notify] "ARP Entry Removed ($lost_mac) -> Notifying $notify"
+		curl -s "$notify"
+	done
+    fi
     if [ -f $dir/.arpignore ]
     then
     	if [[ $(grep -L "$noarp" $dir/.arpignore) ]]; then
-    		lost_entry="$noarp - $(/opt/vyatta/bin/vyatta-op-cmd-wrapper show dhcp leases | grep "$noarp" | awk '{print $6}')"
+    		lost_entry="$noarp - $(grep "$noarp" $dir/.dhcpleases | awk '{print $6}')"
     		logger -t [arp_notify] "ARP Entry Removed -> $lost_entry"
     		echo $lost_entry >> $dir/.arplist
     	fi
     else
-    	lost_entry="$noarp - $(/opt/vyatta/bin/vyatta-op-cmd-wrapper show dhcp leases | grep "$noarp" | awk '{print $6}')"
+    	lost_entry="$noarp - $(grep "$noarp" $dir/.dhcpleases | awk '{print $6}')"
     	logger -t [arp_notify] "ARP Entry Removed -> $lost_entry"
     	echo $lost_entry >> $dir/.arplist
     fi
@@ -74,7 +119,7 @@ else
         logger -t [arp_notify] "ARP table changes were detected. Sending alert."
 		curl -s --form-string "token=$pushover_app_token" \
 				--form-string "user=$pushover_user_token" \
-				--form-string "message=$message" \
+				--form-string "message=[$hostname] $service - $message" \
 				https://api.pushover.net/1/messages.jsonop
     else
         logger -t [arp_notify] "ARP table changes were detected."
@@ -82,4 +127,8 @@ else
 fi
 
 rm $dir/.arplist $dir/.arptable
+if [ -f $dir/.dhcpleases ]
+then
+    rm $dir/.dhcpleases
+fi
 mv $dir/.arptablenew $dir/.arptable
